064ee1b804dec3aba7b6899220a6fddb3a71b81f
solfKwolf <13536812252@163.com>
2026-09-12 00:04:22 +0800

    feat: Maven 项目骨架（Boot 3.5.13 全量依赖）

---
+++ src/main/java/com/workbench/WorkbenchApplication.java
@@ -0,0 +1,12 @@
+package com.workbench;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkbenchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkbenchApplication.class, args);
+    }
+}
